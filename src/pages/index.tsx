48dbdfcc11e661edb7a9be6084aaf0ba7caeb3ad
augustkimo <august.kimo@icloud.com>
2022-07-16 10:03:44 -0400

    fix: remove slow getServerSideProps call

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { loadMovies } from "../helpers";
 import ExploreScreen from "../screens/ExploreScreen";
 import { ExploreApiRespData } from "../types/common";
@@ -8,33 +10,48 @@ type HomeProps = {
   movieData: ExploreApiRespData;
 };
 const Home: NextPage<HomeProps> = (props) => {
-  return (
-    <>
-      <Head>
-        <title>MovieMe - Movie Explorer & Journal</title>
-        <meta name="description" content="MovieMe - the best movie journal and explorer." />
-      </Head>
 
-      <ExploreScreen movieData={props.movieData} />
-    </>
-  );
-};
+  const router = useRouter();
+  const [movieData, setMovieData] = useState<ExploreApiRespData | null>(null);
 
-export async function getServerSideProps() {
-  const movieData = await loadMovies(1, "popular");
-  if (movieData !== null) {
-    return {
-      props: {
-        movieData: movieData,
-      },
-    };
-  } else {
-    return {
-      redirect: {
-        destination: "/500",
-      },
-    };
+  async function loadInitialData() {
+    const movieData = await loadMovies(1, "popular");
+    if (movieData !== null) {
+      setMovieData(movieData);
+    } else {
+      router.push("/500");
+    }
   }
-}
+
+  useEffect(() => {
+    loadInitialData();
+  }, [])
+
+  if(movieData !== null) {
+    return (
+      <>
+        <Head>
+          <title>MovieMe - Movie Explorer & Journal</title>
+          <meta name="description" content="MovieMe - the best movie journal and explorer." />
+        </Head>
+  
+        <ExploreScreen movieData={movieData} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Head>
+          <title>MovieMe - Movie Explorer & Journal</title>
+          <meta name="description" content="MovieMe - the best movie journal and explorer." />
+        </Head>
+  
+        <div className="min-h-screen w-full">
+          Loading...
+        </div>
+      </>
+    );
+  }
+};
 
 export default Home;
